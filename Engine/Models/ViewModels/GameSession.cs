0920f082a18c6bf7a7fb582ad95acf2b6e3b7eef
Oskari Hilekallio <oskuhile@gmail.com>
2020-06-10 14:33:21 +0300

    Added Player and GameSession classes

---
+++ Engine/Models/ViewModels/GameSession.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models.ViewModels
+{
+    class GameSession
+    {
+        public Player CurrentPlayer { get; set; }
+
+        public GameSession()
+        {
+            CurrentPlayer = new Player
+            {
+                Name = "Osku",
+                Gold = 1000000
+            };
+        }
+    }
+   
+}
