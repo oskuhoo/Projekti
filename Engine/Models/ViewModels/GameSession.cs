08c002aae7d3350c1a2a9a4e6e62f6705a8b5879
Oskari Hilekallio <oskuhile@gmail.com>
2020-06-10 15:33:43 +0300

    add the rest of the Player properties

--- Engine/Models/ViewModels/GameSession.cs
+++ Engine/Models/ViewModels/GameSession.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Engine.Models;
 
 namespace Engine.Models.ViewModels
 {
-    class GameSession
+    public class GameSession
     {
         public Player CurrentPlayer { get; set; }
 
@@ -15,6 +16,10 @@ namespace Engine.Models.ViewModels
             CurrentPlayer = new Player
             {
                 Name = "Osku",
+                CharacterClass = "Mage",
+                HitPoints = 1000,
+                ExperiencePoints = 0,
+                Level = 1,
                 Gold = 1000000
             };
         }
