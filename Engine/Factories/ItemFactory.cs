8adf5a5b4a0f55d796a1637f5b8e0531ddb264f0
Oskari Hilekallio <oskuhile@gmail.com>
2020-06-14 20:53:46 +0300

    add GameItem Factory and Weapons

---
+++ Engine/Factories/ItemFactory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Models;
+
+namespace Engine.Factories
+{
+    public static class ItemFactory
+    {
+        private static List<GameItem> stdGameItems;
+
+        static ItemFactory()
+        {
+            stdGameItems = new List<GameItem>();
+
+            //========================ID, NAME, PRICE, MINDMG, MAXDMG, STRIKES=========================
+            stdGameItems.Add(new Weapon(1, "Bronze Sword", 1, 5, 10, 1));
+            stdGameItems.Add(new Weapon(2, "Bronze Daggers", 5, 3, 6, 2));
+            stdGameItems.Add(new Weapon(666, "Owner", 90000, 150000, 200000, 5));
+        }
+
+        public static GameItem CreateGameItem(int itemTypeID)
+        {
+            GameItem stdItem = stdGameItems.FirstOrDefault(item => item.ItemTypeID == itemTypeID);
+
+            if (stdItem != null)
+            {
+                return stdItem.Clone();
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Engine/Models/Weapon.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    public class Weapon : GameItem
+    {
+        public int MinimumDmg { get; set; }
+        public int MaximumDmg { get; set; }
+        public int Strikes { get; set; }
+
+        public Weapon(int itemTypeID, string name, int price, int minDmg, int maxDmg, int strikes) : base(itemTypeID, name, price)
+        {
+            MinimumDmg = minDmg;
+            MaximumDmg = maxDmg;
+            Strikes = strikes;
+        }
+
+        public new Weapon Clone()
+        {
+            return new Weapon(ItemTypeID, Name, Price, MinimumDmg, MaximumDmg, Strikes);
+        }
+    }
+}
